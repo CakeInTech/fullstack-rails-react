f6a7acc49bd497a8b1e34763c88c28e70054dac7
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-05-01 16:02:57 +0200

    - Setup the api & Include the linters configuration

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,2 +1,3 @@
 class Greeting < ApplicationRecord
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'root/index'
+  namespace :api do
+    get '/greetings', to: "greetings#random"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
